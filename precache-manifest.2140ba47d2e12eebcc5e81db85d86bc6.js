42a86a1b2e0e22c1022dcf5795279e20462a9ffc
Bhargav Gohil <bhargavgohil25@gmail.com>
2020-10-02 22:14:01 +0530

    Updates

---
+++ precache-manifest.2140ba47d2e12eebcc5e81db85d86bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19aeeb843932528db81c38cfb88d276f",
+    "url": "/tic_tac_toe/index.html"
+  },
+  {
+    "revision": "c99608a63e7e3eadde7d",
+    "url": "/tic_tac_toe/static/css/main.29349a53.chunk.css"
+  },
+  {
+    "revision": "498e9b04c62191537208",
+    "url": "/tic_tac_toe/static/js/2.07c5fead.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic_tac_toe/static/js/2.07c5fead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99608a63e7e3eadde7d",
+    "url": "/tic_tac_toe/static/js/main.11d68a51.chunk.js"
+  },
+  {
+    "revision": "135ba7b087ac225cebf9",
+    "url": "/tic_tac_toe/static/js/runtime-main.49020b15.js"
+  }
+]);
